4d503789c0149a4ae24eaab2a6a6a11bbe4743ba
Leonard Gärtner <gaertnerleonard@web.de>
2022-08-12 11:40:06 +0200

    add favicon

--- webapp/build.sh
+++ webapp/build.sh
@@ -6,3 +6,4 @@ mkdir ../webserver/static
 cp -r ./src/html ../webserver/static/html
 cp -r ./src/compiled_js ../webserver/static/compiled_js
 cp -r ./src/style ../webserver/static/style
+cp -r ./src/favicon.ico ../webserver/static/favicon.ico
