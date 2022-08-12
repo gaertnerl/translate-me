25e0c9fc717a62513d799d9df52d2b2a4aa8ae8e
Leonard Gärtner <gaertnerleonard@web.de>
2022-08-12 10:35:55 +0200

    webapp: add build script

---
+++ webapp/build.sh
@@ -0,0 +1,6 @@
+npx webpack
+rm -r ../webserver/static
+mkdir ../webserver/static
+cp -r ./src/html ../webserver/static/html
+cp -r ./src/compiled_js ../webserver/static/compiled_js
+cp -r ./src/style ../webserver/static/style
