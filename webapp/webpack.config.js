5c36ad40f69aff961899810b233897014dc249f6
Leonard Gärtner <gaertnerleonard@web.de>
2022-08-12 10:33:13 +0200

    add typescript + webpack configs for webapp

---
+++ webapp/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+
+var config = {
+    mode: "production",
+    devtool: "inline-source-map",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader"
+            }
+        ]
+    }
+};
+
+var gameConfig = Object.assign({}, config, {
+    entry: {
+        main: "./src/script/game.ts",
+    },
+    output: {
+        path: path.resolve(__dirname, './src/compiled_js'),
+        filename: "game.js"
+    }
+});
+
+module.exports = [
+    gameConfig,
+];
