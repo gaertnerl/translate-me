5116c68780010ca85016742c9deb5879b5a5cf70
Leonard Gärtner <gaertnerleonard@web.de>
2022-08-15 15:38:30 +0200

    webapp: add dom lib code

--- webapp/src/script/lib/dom.ts
+++ webapp/src/script/lib/dom.ts
@@ -1,7 +1,44 @@
 const userTranslationId = "user-translation"
 const originalText = "original-text"
 const submitTranslationButtonId = "submit-translation-button"
-const scoreId = "score"
+const scoreClass = "score"
+const score500Class = "score-500"
+const score100Class = "score-100"
+const score0Class = "score-0"
+const scoreListId = "score-list"
+const totalScoreId = "total-score-num"
+
+
+function createScore500Element(): HTMLElement | null {
+    const div = document.createElement('div');
+    div.classList.add(scoreClass);
+    div.classList.add(score500Class);
+    const text = document.createTextNode("Perfect! 🤩");
+    div.appendChild(text);
+    return div
+}
+
+function createScore100Element(): HTMLElement | null {
+    const div = document.createElement('div');
+    div.classList.add(scoreClass);
+    div.classList.add(score100Class);
+    const text = document.createTextNode("Good 😊");
+    div.appendChild(text);
+    return div
+}
+
+function createScore0Element(): HTMLElement | null {
+    const div = document.createElement('div');
+    div.classList.add(scoreClass);
+    div.classList.add(score0Class);
+    const text = document.createTextNode("Wrong 🙃");
+    div.appendChild(text);
+    return div
+}
+
+function getScoreListElement(): HTMLElement | null {
+    return document.getElementById(scoreListId)
+}
 
 function getSubmitTranslationButton(): HTMLElement | null {
     return document.getElementById(submitTranslationButtonId)
@@ -15,15 +52,16 @@ function getUserTranslationTextarea(): HTMLElement | null {
     return document.getElementById(userTranslationId)
 }
 
-function getScoreElement(): HTMLElement | null {
-    return document.getElementById(scoreId)
+function getTotalScoreElement(): HTMLElement | null {
+    return document.getElementById(totalScoreId)
 }
 
-const elements: { [key: string]: HTMLElement | null } = {
+const elements: { [key: string]: (HTMLElement | null) } = {
     originalText: getOriginalText(),
     submitTranslationButton: getSubmitTranslationButton(),
     userTranslationTextarea: getUserTranslationTextarea(),
-    scoreElement: getScoreElement()
+    scoreList: getScoreListElement(),
+    totalScore: getTotalScoreElement()
 }
 
-export { elements }
+export { elements, createScore500Element, createScore100Element, createScore0Element }
