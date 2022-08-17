1a2f19eed39aa250c690f3751d51ec30d2debd2c
Leonard Gärtner <gaertnerleonard@web.de>
2022-08-17 20:53:04 +0200

    rename folders

--- similarity-api/build_deploy.sh
+++ similarity-api/build_deploy.sh
@@ -2,4 +2,4 @@
 
 docker build -t similarity-api .
 docker rm similarity-api-instance
-docker run -p 8080:8000 --name similarity-api-instance-0 similarity-api
+docker run -p 8080:8000 --name similarity-api-instance similarity-api
