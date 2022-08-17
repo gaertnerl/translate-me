1f1e33983fc4905f61e9bf0b3e68b1ee13ab3436
Leonard Gärtner <gaertnerleonard@web.de>
2022-08-17 20:09:40 +0200

    similarity-api: work with env vars

--- similarity-api/src/api.py
+++ similarity-api/src/api.py
@@ -1,15 +1,30 @@
+import os
+from loadbalancing.register import register
 from fastapi import FastAPI
 from similarity.similarity_calc import calc_similarity
 
-
-app = FastAPI()
-
-
-def create_response(similarity_score: float) -> dict:
-    return {"similarity": similarity_score}
+_VAR_API_PORT = "API_PORT"
+_VAR_API_HOST = "API_HOST"
+_VAR_LOADBALANCER_REGISTER_URL = "LOADBALANCER_REGISTER_URL"
+_VAR_LOADBALANCER_API_KEY = "LOADBALANCER_API_KEY"
 
 
-@app.get("/similarity/{sentence_1}/{sentence_2}")
-def similarity(sentence_1: str, sentence_2: str):
-    score = calc_similarity(sentence_1, sentence_2)
-    return create_response(score)
+LOADBALANCER_REGISTER_URL = os.environ[_VAR_LOADBALANCER_REGISTER_URL]
+LOADBALANCER_API_KEY = os.environ[_VAR_LOADBALANCER_API_KEY]
+API_HOST = os.environ[_VAR_API_HOST]
+API_PORT = os.environ[_VAR_API_PORT]
+
+
+if __name__ == '__main__':
+
+    register()
+
+    app = FastAPI()
+
+    def create_response(similarity_score: float) -> dict:
+        return {"similarity": similarity_score}
+
+    @app.get("/similarity/{sentence_1}/{sentence_2}")
+    def similarity(sentence_1: str, sentence_2: str):
+        score = calc_similarity(sentence_1, sentence_2)
+        return create_response(score)
