b9d8e7aae81ac6d96de895c5caeb73a3cfeac286
Leonard Gärtner <gaertnerleonard@web.de>
2022-08-18 11:23:35 +0200

    webapp: initialize load balancing with empty load balancer

--- webapp/backend/src/services/services.go
+++ webapp/backend/src/services/services.go
@@ -2,7 +2,6 @@ package services
 
 import (
 	"github.com/gaertnerl/translate-me.git/webserver/lib/caching"
-	"github.com/gaertnerl/translate-me.git/webserver/lib/communication"
 	lb "github.com/gaertnerl/translate-me.git/webserver/lib/load_balancing"
 	"github.com/gaertnerl/translate-me.git/webserver/lib/sentence"
 	"github.com/gaertnerl/translate-me.git/webserver/lib/similarity/service"
@@ -10,7 +9,7 @@ import (
 
 // calculation of sentence similarity is slow, therefor use load balanced similarity service that allows distributing the
 // similarity calculation requests to multiple external instances
-var similarityServiceLoadBalancer lb.LoadBalancer[service.ServiceIdentifier] = lb.New_RandomLB(service.ServiceIdentifier{Protocol: communication.HTTP, Host: "localhost"})
+var similarityServiceLoadBalancer lb.LoadBalancer[service.ServiceIdentifier] = lb.New_RandomLB[service.ServiceIdentifier]()
 var SimilarityService service.SimilarityService = service.New_LoadBalancedWebBasedSimilarityService(similarityServiceLoadBalancer)
 
 // calculation of sentence similarity is slow, therefor
