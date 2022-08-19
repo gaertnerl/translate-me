d52686e8d7cb1362d2c82f385afb09304d45bb69
Leonard Gärtner <gaertnerleonard@web.de>
2022-08-19 13:49:11 +0200

    backend: add request, response types for handlers

---
+++ webapp/backend/src/handlers/request_types.go
@@ -0,0 +1,16 @@
+package handlers
+
+import "github.com/gaertnerl/translate-me.git/webserver/lib/sentence"
+
+type SentenceSimilarityReq struct {
+	SentenceA string `url:"sentence_a"`
+	SentenceB string `url:"sentence_b"`
+}
+
+type RegisterSimilarityEndpointReq struct {
+	Protocol string
+	Port     string
+	Key      string
+}
+
+type UserTranslationRequest = sentence.UserTranslation
---
+++ webapp/backend/src/handlers/response_types.go
@@ -0,0 +1,5 @@
+package handlers
+
+import "github.com/gaertnerl/translate-me.git/webserver/lib/sentence"
+
+type UserTranslationEvaluationResponse = sentence.UserTranslationEvaluation
