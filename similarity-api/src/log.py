8a888644fdaed12f5308fb51188f9531cb4e1e5c
Leonard Gärtner <gaertnerleonard@web.de>
2022-08-19 13:44:12 +0200

    similarity-api: update logger

--- similarity-api/src/log.py
+++ similarity-api/src/log.py
@@ -4,8 +4,13 @@ _LOGGER_NAME = "api-logger"
 
 
 def _setup_custom_logger():
+    format = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler = logging.StreamHandler()
+    handler.setFormatter(format)
     logger = logging.getLogger(_LOGGER_NAME)
     logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
     return logger
 
 
