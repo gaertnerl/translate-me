29a60245736203018dcd18be68d7ac62ba63dc5e
Leonard Gärtner <gaertnerleonard@web.de>
2022-08-19 13:46:30 +0200

    similarity-api: add logging

--- similarity-api/src/similarity/embedder.py
+++ similarity-api/src/similarity/embedder.py
@@ -1,4 +1,7 @@
+import logging
+from time import perf_counter, perf_counter_ns, time
 from similarity.custom_types import Embedding
+from log import logger
 import tensorflow_hub as hub
 import tensorflow_text
 from threading import Lock
@@ -9,8 +12,13 @@ class CachedEmbedder:
 
     def __init__(self, cache_size: int) -> None:
         self.__c = {}
+        logger.log(logging.DEBUG, "start loading encoder")
+        start = perf_counter()
         self.__embed = hub.load(
             "https://tfhub.dev/google/universal-sentence-encoder-multilingual/3")
+        end = perf_counter()
+        logger.log(logging.DEBUG,
+                   f"finished loading encoder, took {end-start} seconds.")
         self.__lock = Lock()
         self.__cache_size = cache_size
 
