d5a243f2b4826e89dcb6503b8a35f4fd3d1d116a
Leonard Gärtner <gaertnerleonard@web.de>
2022-08-19 13:40:41 +0200

    similarity-api: update buid deploy script

--- similarity-api/build_deploy.sh
+++ similarity-api/build_deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-docker build -t similarity-api .
+docker build -t similarity-api --build-arg API_HOST=0.0.0.0 --build-arg API_PORT=8000 --build-arg LOADBALANCER_API_KEY='key' --build-arg LOADBALANCER_REGISTER_URL='http://0.0.0.0/api/register' .
 docker rm similarity-api-instance
 docker run -p 8080:8000 --name similarity-api-instance similarity-api
